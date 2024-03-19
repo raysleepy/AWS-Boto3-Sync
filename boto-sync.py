b7041720436d8863ae07b78c0cb60e01405f3f05
ray sleepy <git@raysleepy.com>
2024-03-19 00:43:14 -0400

    Implemented 2 check modes: last run timestamp, timestamp diff

--- boto-sync.py
+++ boto-sync.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+from datetime import datetime, timezone
 from enum import Enum, IntEnum, auto
 import hashlib
 import json
@@ -15,7 +15,7 @@ class Sizes(IntEnum):
 class Checks(Enum):
     LAST_RUN_TIMESTAMP = auto()
     FILE_EXISTS = auto()
-    FILE_TIMESTAMP = auto()
+    FILE_TIMESTAMP_DIFFERS = auto()
     FILE_HASH = auto()
 
 class Configs(Enum):
@@ -30,27 +30,40 @@ class Configs(Enum):
         max_concurrency = 1,
     )
     HASH_CHUNK_SIZE = 100 * Sizes.MB
-    CHECK_MODE = Checks.FILE_EXISTS
+    CHECK_MODE = Checks.FILE_TIMESTAMP_DIFFERS
     LOG_LEVEL = logging.INFO
     DATA_DIR = 'data'
     TMP_DIR = 'tmp'
     TEST_MODE = False
+    MAX_FILE_COUNT = 9999
+    MAX_TIMESTAMP_DELTA_IN_SECONDS = 10
 
 logging.basicConfig(encoding='utf-8', format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y/%m/%d %H::%M:%S', level=Configs.LOG_LEVEL.value)
 logger = logging.getLogger(__name__)
 
-def serialize_datetime(obj): 
-    if isinstance(obj, datetime.datetime): 
+ts_file_path = os.path.join(Configs.DATA_DIR.value, 'last_run')
+
+lastrun_ts = datetime(1971, 1, 1, 0, 0, 0)
+if os.path.exists(ts_file_path):
+    try:
+        with open(ts_file_path, 'r+') as f:
+            lastrun_ts = datetime.strptime(f.readline(), "%Y-%m-%d %H:%M:%S%z")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+logger.info(f"Last run timestamp: {lastrun_ts}")
+
+def serialize_datetime(obj: object): 
+    if isinstance(obj, datetime): 
         return obj.isoformat() 
     raise TypeError("Type not serializable")
 
-def get_file_hash(file_path):
+def get_file_hash(file_path: str):
     with open(file_path, 'rb') as f:
         hash = hashlib.md5()
         while chunk := f.read(Configs.HASH_CHUNK_SIZE.value):
             hash.update(chunk)
     digest = hash.hexdigest()
-    logger.info(f"md5 hash for {file_path} is {digest}")
+    logger.info(f"File hash for {file_path} is {digest}")
     return digest
 
 def update_file_hash(file_path: str, dst_bucket: str, dst_key: str):
@@ -59,28 +72,59 @@ def update_file_hash(file_path: str, dst_bucket: str, dst_key: str):
     # TODO
     pass
 
-def need_to_sync(item: object, dst_prefix: str, dst_keys: list[str]) -> bool:
+def update_timestamp():
+    now = datetime.now(tz=timezone.utc).replace(microsecond=0)
+    try:
+        with open(ts_file_path, 'w+') as f:
+            f.write(str(now))
+        logger.info(f"Updated last run timestamp to {now}")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+def need_to_sync(item: object, dst_prefix: str, dst_keys: list[str], dst: list[object]) -> bool:
     src_key = item['Key']
     logger.debug(f"src_key: {src_key}")
     logger.debug(f"dst_prefix: {dst_prefix}")
     logger.debug(f"dst_keys: {dst_keys}")
-
+    logger.debug(f"item:\n{json.dumps(item, default=serialize_datetime, indent=4)}\n")
+    
     match Configs.CHECK_MODE.value:
         case Checks.LAST_RUN_TIMESTAMP:
-            # TODO
-            return False
+            src_ts = datetime.strptime(str(item['LastModified']), "%Y-%m-%d %H:%M:%S%z")
+            if src_ts > lastrun_ts:
+                logger.info(f"{src_key} last modified {src_ts} > last run {lastrun_ts} => need to copy")
+                return True
+            else:
+                logger.info(f"{src_key} last modified {src_ts} < last run {lastrun_ts} => skipping")
+                return False
 
         case Checks.FILE_EXISTS:
             if dst_prefix + src_key in dst_keys:
-                logger.info("File exists. Skip syncing")
+                logger.info("File exists => skipping")
                 return False
             else:
-                logger.info("File does not exist. Need to sync")
+                logger.info("File does not exist => need to copy")
                 return True
 
-        case Checks.FILE_TIMESTAMP:
-            # TODO
-            return False
+        case Checks.FILE_TIMESTAMP_DIFFERS:
+            dst_key = dst_prefix + src_key
+            if dst_key in dst_keys:
+                logger.info("File exists")
+                src_ts = datetime.strptime(str(item['LastModified']), "%Y-%m-%d %H:%M:%S%z")
+                dst_ts = datetime.strptime(str([x['LastModified'] for x in dst['Contents'] if x['Key'] == dst_key][0]), "%Y-%m-%d %H:%M:%S%z")
+                delta = int((src_ts - dst_ts).total_seconds())
+                max = Configs.MAX_TIMESTAMP_DELTA_IN_SECONDS.value
+                logger.info(f"src: {src_key} last modified {src_ts}")
+                logger.info(f"dst: {dst_key} last modified {dst_ts}")
+                if delta >= max:
+                    logger.info(f"src - dst = {delta} seconds >= {max} => need to copy")
+                    return True
+                else:
+                    logger.info(f"src - dst = {delta} seconds < {max} => skipping")
+                    return False
+            else:
+                logger.info("File does not exist => need to copy")
+                return True
 
         case Checks.FILE_HASH:
             # TODO
@@ -98,7 +142,7 @@ def sync_one_bucket(src_profile: str, src_bucket: str, dst_profile: str, dst_buc
     logger.debug(f"List of source bucket s3://{dst_bucket}:\n{json.dumps(dst, default=serialize_datetime, indent=4)}\n")
 
     if any('Contents' in x for x in dst):
-        dst_keys = [x['Key'] for x in dst['Contents'] if x['Key'].startswith(dst_prefix) ]
+        dst_keys = [x['Key'] for x in dst['Contents'] if x['Key'].startswith(dst_prefix)]
         logger.debug(f"List of keys for destination s3://{dst_bucket}/{dst_prefix}:\n{dst_keys}")
 
     src_keys = []
@@ -110,20 +154,21 @@ def sync_one_bucket(src_profile: str, src_bucket: str, dst_profile: str, dst_buc
         logger.debug(f"List of keys for source s3://{src_bucket}/:\n{src_keys}")
 
     if any('Contents' in x for x in src):
-        for item in src['Contents']:
+        for item in src['Contents'][0:min(len(src['Contents']), Configs.MAX_FILE_COUNT.value)]:
             src_key = item['Key']
             dst_key = f"{dst_prefix}{src_key}"
             filename = src_key.split('/')[-1]
             file_path = os.path.join(Configs.DATA_DIR.value, Configs.TMP_DIR.value, filename)
 
             logger.info(f"Evaluating s3://{src_bucket}/{src_key}")
-            if need_to_sync(item, dst_prefix, dst_keys):
+            logger.debug(json.dumps(item, default=serialize_datetime, indent=4))
+            if need_to_sync(item, dst_prefix, dst_keys, dst):
                 try:
                     logger.info(f"Downloading s3://{src_bucket}/{src_key} to {file_path}")
                     s3_src.download_file(src_bucket, src_key, file_path)
                     
                     if Configs.TEST_MODE.value:
-                        logger.info("Test mode. Skip uploading the file")
+                        logger.info("Test mode => skipping")
                     else:
                         logger.info(f"Uploading {filename} to s3://{dst_bucket}/{dst_key}")
                         s3_dst.upload_file(file_path, dst_bucket, dst_key, Config=Configs.S3_TX_CONFIGS.value)
@@ -157,6 +202,9 @@ def main():
         logger.info(f"Working on s3://{bucket['src_bucket']}\n")
         sync_one_bucket(src_profile=bucket['src_profile'], src_bucket=bucket['src_bucket'],
                         dst_profile=bucket['dst_profile'], dst_bucket=bucket['dst_bucket'], dst_prefix=bucket['src_bucket'] + '/')
+
+    if not Configs.TEST_MODE.value and Configs.CHECK_MODE.value is Checks.LAST_RUN_TIMESTAMP:
+        update_timestamp()
 
 if __name__ == "__main__":
     main()
